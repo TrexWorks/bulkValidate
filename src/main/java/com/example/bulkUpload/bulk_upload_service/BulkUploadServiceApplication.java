64a2946934680f95ff244e045323382bc179b74f
Mandeep singh <mandeep.singh@freightfox.ai>
2025-08-16 15:29:12 +0530

    Project setup

---
+++ src/main/java/com/example/bulkUpload/bulk_upload_service/BulkUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulkUpload.bulk_upload_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkUploadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bulkUpload/bulk_upload_service/BulkUploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bulkUpload.bulk_upload_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkUploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
